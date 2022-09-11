6a9030cbb3ebab9f25f5018a850c60eb55d15d1b
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2022-09-12 00:29:33 +0300

    Add files via upload

---
+++ AngryBirdClone/AngryBirdClone/GameScene.swift
@@ -0,0 +1,253 @@
+//
+//  GameScene.swift
+//  AngryBirdClone
+//
+//  Created by Berkay Kuzu on 11.09.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    //var bird2 = SKSpriteNode()
+    
+    var bird = SKSpriteNode()
+    var box1 = SKSpriteNode()
+    var box2 = SKSpriteNode()
+    var box3 = SKSpriteNode()
+    var box4 = SKSpriteNode()
+    var box5 = SKSpriteNode()
+    
+    var gameStarted = false // if gameStarted false, the user can pull, otherwise, not!
+    var originalPosition : CGPoint?
+    
+    var score = 0
+    var scoreLabel = SKLabelNode()
+    
+    
+    enum ColliderType : UInt32 {
+        case Bird = 1
+        case Box = 2
+    }
+    
+    
+    override func didMove(to view: SKView) {
+        /*
+        let texture = SKTexture(imageNamed: "bird")
+        bird2 = SKSpriteNode(texture: texture)
+        bird2.position = CGPoint(x: -self.frame.width / 4, y: -self.frame.height / 4)
+        bird2.size = CGSize(width: self.frame.width / 16, height: self.frame.height / 10)
+        bird2.zPosition = 1
+        self.addChild(bird2)
+         These codes show how you can create a bird with code, but we can continue with the design.
+        */
+        
+        //Physics Body
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame) // We created a total frame world in GameScene!.
+        self.scene?.scaleMode = .aspectFit
+        self.physicsWorld.contactDelegate = self // to recognize the collision!
+        
+        //Bird
+        
+        bird = childNode(withName: "bird") as! SKSpriteNode
+        
+        let birdTexture = SKTexture(imageNamed: "bird")
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 13)
+        bird.physicsBody?.affectedByGravity = false
+        bird.physicsBody?.isDynamic = true
+        bird.physicsBody?.mass = 0.15
+        originalPosition = bird.position
+        
+        bird.physicsBody?.contactTestBitMask = ColliderType.Bird.rawValue
+        bird.physicsBody?.categoryBitMask = ColliderType.Bird.rawValue
+        bird.physicsBody?.collisionBitMask = ColliderType.Box.rawValue
+        
+        //Box
+        let boxTexture = SKTexture(imageNamed: "brick")
+        let size = CGSize(width: boxTexture.size().width / 6, height: boxTexture.size().height / 6)
+        
+        box1 = childNode(withName: "box1") as! SKSpriteNode
+        box1.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box1.physicsBody?.affectedByGravity = true
+        box1.physicsBody?.isDynamic = true
+        box1.physicsBody?.mass = 0.4
+        
+        box1.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+        box2 = childNode(withName: "box2") as! SKSpriteNode
+        box2.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box2.physicsBody?.affectedByGravity = true
+        box2.physicsBody?.isDynamic = true
+        box2.physicsBody?.mass = 0.4
+        
+        box2.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+        box3 = childNode(withName: "box3") as! SKSpriteNode
+        box3.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box3.physicsBody?.affectedByGravity = true
+        box3.physicsBody?.isDynamic = true
+        box3.physicsBody?.mass = 0.4
+        
+        box3.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+        box4 = childNode(withName: "box4") as! SKSpriteNode
+        box4.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box4.physicsBody?.affectedByGravity = true
+        box4.physicsBody?.isDynamic = true
+        box4.physicsBody?.mass = 0.4
+        
+        box4.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+        box5 = childNode(withName: "box5") as! SKSpriteNode
+        box5.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box5.physicsBody?.affectedByGravity = true
+        box5.physicsBody?.isDynamic = true
+        box5.physicsBody?.mass = 0.4
+
+        box5.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+        //Label (We create it with code!)
+        
+        scoreLabel.fontName = "Helvetica"
+        scoreLabel.fontSize = 60
+        scoreLabel.text = "0"
+        scoreLabel.position = CGPoint(x: 0, y: self.frame.height / 4)
+        scoreLabel.zPosition = 2
+        self.addChild(scoreLabel)
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.collisionBitMask == ColliderType.Bird.rawValue || contact.bodyB.collisionBitMask == ColliderType.Bird.rawValue {
+            
+            score += 1
+            scoreLabel.text = String(score)
+            
+        }
+            
+            
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+      
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+      
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /*
+        bird.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 100))
+        bird.physicsBody?.affectedByGravity = true
+         */
+        
+        if gameStarted == false {
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchNodes = nodes(at: touchLocation)
+                
+                if touchNodes.isEmpty == false {
+                    for node in touchNodes {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == bird {
+                                bird.position = touchLocation
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if gameStarted == false {
+            
+            if let touch = touches.first { // if we get touch, do the rest!
+                
+                let touchLocation = touch.location(in: self) // where the users touch
+                let touchNodes = nodes(at: touchLocation) // dokunulan node'u aldım, touchlocation'ı bana ver!
+                
+                if touchNodes.isEmpty == false { // if there is an touchNode, touched point, do the rest!
+                    
+                    for node in touchNodes { // by using for loop, we took all touched points.
+                        
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == bird {
+                                bird.position = touchLocation // the new position of the bird is the touched point!
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if gameStarted == false {
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchNodes = nodes(at: touchLocation)
+                
+                if touchNodes.isEmpty == false {
+                    for node in touchNodes {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == bird {
+                               
+                                let dx = -(touchLocation.x - originalPosition!.x)
+                                let dy = -(touchLocation.y - originalPosition!.y)
+                                
+                                let impulse = CGVector(dx: dx, dy: dy)
+                                
+                                bird.physicsBody?.applyImpulse(impulse)
+                                bird.physicsBody?.affectedByGravity = true
+                                
+                                gameStarted = true
+                                
+                            
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        if let birdPhysicsBody = bird.physicsBody { // bird.physicsBody is not optional anymore!
+            // angularVelocity : açısal hız, velocity: hız
+            
+            if birdPhysicsBody.velocity.dx <= 0.1 && birdPhysicsBody.velocity.dy <= 0.1 && birdPhysicsBody.angularVelocity <= 0.1 && gameStarted == true {
+                
+                bird.physicsBody?.affectedByGravity = false
+                bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                bird.physicsBody?.angularVelocity = 0
+                bird.zPosition = 1
+                bird.position = originalPosition!
+                
+                score = 0
+                scoreLabel.text = String(score)
+                
+                gameStarted = false
+                
+            }
+            
+        }
+        
+    }
+}
---
+++ AngryBirdClone/AngryBirdClone/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  AngryBirdClone
+//
+//  Created by Berkay Kuzu on 11.09.2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsPhysics = true // Blue circle around the bird. This is a physical body.
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
